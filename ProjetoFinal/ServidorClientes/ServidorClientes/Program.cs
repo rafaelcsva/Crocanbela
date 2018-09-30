1b6937ae43e0ed3ca8448d735ebd3205ff91a61d
rafaelcastro141 <rafaelgyncastro63@gmail.coM>
2018-09-30 08:51:50 -0300

    adicionando projeto c# para microservico relacionado ao cliente

---
+++ ProjetoFinal/ServidorClientes/ServidorClientes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ServidorNomes;
+using Grpc.Core;
+
+namespace ServidorClientes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			const string host = "localhost";
+			const int port = 1808;
+
+			Channel channel = new Channel(host + ":" + port.ToString(), ChannelCredentials.Insecure);
+
+			var client = new Nomes.NomesClient(channel);
+
+			var resp = client.Cadastrar(new RegistroServico { Host = "localhost", Porta = 1808, Servico = "Clientes" });
+			Console.WriteLine(resp.Host + " recebeu ok!");
+        }
+    }
+}
